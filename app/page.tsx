b959623b8a11abe702cbf4088010f2e0c7ec6c8d
Zinger Marat <zingermarat@gmail.com>
2025-01-10 11:22:14 +0200

    basic markuo created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
 import Image from "next/image";
-import {Button} from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import {
+  Container,
+  Title,
+  Categories,
+  SortPopup,
+  TopBar,
+  Filters,
+} from "@/components/shared";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <Container className="mt-5">
+        <Title text="All pizzas" size="lg" className="font=extrabold"></Title>
+      </Container>
+      <TopBar />
+      <Container className="pb-14">
+        <div className="flex gap-[60px]">
+          {/*Filter*/}
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+
+          {/*Products*/}
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">Pizzas list</div>
+          </div>
+        </div>
+      </Container>
+    </>
   );
 }
