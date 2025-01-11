70af4d4fc6e27dc84702733f26e0281705d5883b
Zinger Marat <zingermarat@gmail.com>
2025-01-11 21:21:00 +0200

    the database was connected

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -21,6 +21,7 @@ const cats = [
 
 export const Categories: React.FC<Props> = ({ className }) => {
   const categoryActiveId = useCategoryStore((state) => state.activeId);
+
   return (
     <div className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}>
       {cats.map(({ name, id }, index) => (
