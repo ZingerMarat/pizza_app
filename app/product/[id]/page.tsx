be33ff0ae89b3710878ca4c607e965c07020bdf7
Zinger Marat <zingermarat@gmail.com>
2025-01-22 20:56:21 +0200

    info page are created

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,3 +1,35 @@
-export default function ProductPage({ params: { id } }: { params: { id: string } }) {
-  return <p> Product {id} </p>;
+import { Container, Title } from "@/components/shared";
+import { ProductImage } from "@/components/shared";
+import { GroupVariants } from "@/components/shared/group-variants";
+import { prisma } from "@/prisma/prisma-client";
+import { notFound } from "next/navigation";
+
+export default async function ProductPage({ params: { id } }: { params: { id: string } }) {
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <Container className="flex flex-col my-20">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} className="my-20" size={20} />
+
+        <div className="w-[490px] bg-[#FCFCFC] p-7">
+          <Title className="font-extrabold mb-1" text={product.name} size="md"></Title>
+          <p className="text-gray-400">bla bla bla</p>
+
+          <GroupVariants
+            items={[
+              { name: "small", value: "1" },
+              { name: "medium", value: "2" },
+              { name: "large", value: "3" },
+            ]}
+            selectedValue="2"
+          ></GroupVariants>
+        </div>
+      </div>
+    </Container>
+  );
 }
