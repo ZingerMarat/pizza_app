d793842cef4fda8993b79f3d43151262cac07a4d
Zinger Marat <zingermarat@gmail.com>
2025-02-04 13:09:05 +0200

    Add empty cart state to CartDrawer with image and message

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -2,14 +2,16 @@
 
 import React, { PropsWithChildren } from "react";
 import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from "@/shared/components/ui/sheet";
-import { Arrow } from "@radix-ui/react-popover";
-import { Button } from "../ui";
+import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 import { CartDrawerItem } from "./cart-drawer-item";
 import { getCartItemDetails } from "@/shared/lib";
 import { useCartStore } from "@/shared/store";
 import { PizzaSize, PizzaType } from "@/shared/constants/pizza";
+import { Title } from "./title";
+import { Button } from "../ui";
+import { cn } from "@/shared/lib/utils";
 
 interface Props {
   className?: string;
@@ -35,54 +37,77 @@ export const CartDrawer: React.FC<PropsWithChildren<Props>> = ({ children, class
     <Sheet>
       <SheetTrigger asChild>{children}</SheetTrigger>
       <SheetContent className="flex flex-col justify-between pb-0 bg-[#F4F1EE]">
-        <SheetHeader>
-          <SheetTitle>
-            In the cart{" "}
-            <span className="font-bold">
-              {items.length} {items.length === 1 ? "item" : "items"}
-            </span>
-          </SheetTitle>
-        </SheetHeader>
+        <div className={cn("flex flex-col h-full", !totalAmount && "justify-center")}>
+          {totalAmount > 0 && (
+            <SheetHeader>
+              <SheetTitle>
+                In the cart{" "}
+                <span className="font-bold">
+                  {items.length} {items.length === 1 ? "item" : "items"}
+                </span>
+              </SheetTitle>
+            </SheetHeader>
+          )}
 
-        {/* Cart items */}
-        <div className="-mx-6 mt-5 overflow-auto flex-1">
-          {items.map((item) => (
-            <div key={item.id} className="mb-2">
-              <CartDrawerItem
-                id={item.id}
-                disabled={item.disabled}
-                imageUrl={item.imageUrl}
-                name={item.name}
-                price={item.price}
-                quantity={item.quantity}
-                details={item.pizzaSize && item.pizzaType ? getCartItemDetails(item.ingredients, item.pizzaType as PizzaType, item.pizzaSize as PizzaSize) : ""}
-                onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
-                onClickRemove={() => removeCartItem(item.id)}
-              />
+          {!totalAmount && (
+            <div className="flex flex-col items-center justify-center w-72 mx-auto">
+              <Image src="/assets/images/empty-box.png" alt="Empty box" width={120} height={120} />
+              <Title size="sm" text="Your cart is empty" className="text-center font-bold my-2" />
+              <p className="text-center text-neutral-500 mb-5">Looks like you haven't added anything to your cart yet</p>
+
+              <SheetClose>
+                <Button size="lg" className="w-56 h-12 text-base">
+                  <ArrowLeft className="w-5 ml-2" />
+                  Back to menu
+                </Button>
+              </SheetClose>
             </div>
-          ))}
+          )}
+
+          {/* Cart items */}
+          {totalAmount > 0 && (
+            <>
+              <div className="-mx-6 mt-5 overflow-auto flex-1">
+                {items.map((item) => (
+                  <div key={item.id} className="mb-2">
+                    <CartDrawerItem
+                      id={item.id}
+                      disabled={item.disabled}
+                      imageUrl={item.imageUrl}
+                      name={item.name}
+                      price={item.price}
+                      quantity={item.quantity}
+                      details={item.pizzaSize && item.pizzaType ? getCartItemDetails(item.ingredients, item.pizzaType as PizzaType, item.pizzaSize as PizzaSize) : ""}
+                      onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
+                      onClickRemove={() => removeCartItem(item.id)}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <SheetFooter className="-mx-6 bg-white p-8">
+                <div className="w-full">
+                  <div className="flex mb-4">
+                    <span className="flex flex-1 text-lg text-neutral-500">
+                      Total
+                      <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
+                    </span>
+
+                    <span className="font-bold text-lg">{totalAmount} $</span>
+                  </div>
+
+                  <Link href="/card">
+                    <Button type="submit" className="w-full h-12 text-base">
+                      {" "}
+                      Order
+                      <ArrowRight className="w-5 ml-2" />
+                    </Button>
+                  </Link>
+                </div>
+              </SheetFooter>
+            </>
+          )}
         </div>
-
-        <SheetFooter className="-mx-6 bg-white p-8">
-          <div className="w-full">
-            <div className="flex mb-4">
-              <span className="flex flex-1 text-lg text-neutral-500">
-                Total
-                <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
-              </span>
-
-              <span className="font-bold text-lg">{totalAmount} $</span>
-            </div>
-
-            <Link href="/card">
-              <Button type="submit" className="w-full h-12 text-base">
-                {" "}
-                Order
-                <ArrowRight className="w-5 ml-2" />
-              </Button>
-            </Link>
-          </div>
-        </SheetFooter>
       </SheetContent>
     </Sheet>
   );
