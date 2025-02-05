8cb887143c96f6cb1ebfe69094138ce077d42361
Zinger Marat <zingermarat@gmail.com>
2025-02-05 23:40:23 +0200

    Implement checkout page layout and components, including CheckoutItemDetails and WhiteBlock

---
+++ app/(checkout)/checkout/page.tsx
@@ -0,0 +1,77 @@
+import { CheckoutItemDetails, Container, Title, WhiteBlock } from "@/shared/components/shared";
+import { Button, Input, Textarea } from "@/shared/components/ui";
+import { ArrowRight, Package, Percent, Truck } from "lucide-react";
+
+export default function CheckoutPage() {
+  return (
+    <Container className="mt-10">
+      <Title text="Checkout" size="lg" className="font-extrabold mb-8" />
+
+      <div className="flex gap-10">
+        {/* Left side */}
+        <div className="flex flex-col gap-10 flex-1 mb-20">
+          <WhiteBlock title="Cart"></WhiteBlock>
+
+          <WhiteBlock title="Personal information">
+            <div className="grid grid-cols-2 gap-5">
+              <Input name="firstName" className="text-base" placeholder="First name" />
+              <Input name="lastName" className="text-base" placeholder="Last name" />
+              <Input name="email" className="text-base" placeholder="Email" />
+              <Input name="phone" className="text-base" placeholder="Phone" />
+            </div>
+          </WhiteBlock>
+
+          <WhiteBlock title="Delivery information">
+            <div className="flex flex-col gap-5">
+              <Input name="Address" className="text-base" placeholder="Address" />
+              <Textarea name="address" className="text-base" placeholder="Comment" rows={5} />
+            </div>
+          </WhiteBlock>
+        </div>
+
+        {/* Right side */}
+        <div className="w-[450px]">
+          <WhiteBlock className="p-6 sticky top-4">
+            <div className="flex flex-col gap-1">
+              <span className="text-xl">Total:</span>
+              <span className="text-[34px] font-extrabold">10$</span>
+            </div>
+
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Package size={18} className="mr-2 text-gray-300" />
+                  Subtotal
+                </div>
+              }
+              value="10"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Percent size={18} className="mr-2 text-gray-300" />
+                  Tax
+                </div>
+              }
+              value="2"
+            />
+            <CheckoutItemDetails
+              title={
+                <div className="flex items-center">
+                  <Truck size={18} className="mr-2 text-gray-300" />
+                  Delivery
+                </div>
+              }
+              value="5"
+            />
+
+            <Button type="submit" className="w-full g-14 rounded-2xl mt-6 text-base font-bold">
+              Checkout
+              <ArrowRight className="w-5 ml-2" />
+            </Button>
+          </WhiteBlock>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- shared/components/shared/header.tsx
+++ shared/components/shared/header.tsx
@@ -5,18 +5,20 @@ import Image from "next/image";
 import { cn } from "@/shared/lib/utils";
 import { Container } from "./container";
 import { Button } from "../ui";
-import { ArrowRight, ShoppingCart, User } from "lucide-react";
+import { User } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
 import { CartButton } from "./cart-button";
 
 interface Props {
+  hasSearch?: boolean;
+  hasCart?: boolean;
   className?: string;
 }
 
-export const Header: React.FC<Props> = ({ className }) => {
+export const Header: React.FC<Props> = ({ hasSearch = true, hasCart = true, className }) => {
   return (
-    <header className={cn("border border-b", className)}>
+    <header className={cn("border-b", className)}>
       <Container className="flex items-center justify-between py-8">
         {/* Left side */}
         <Link href="/">
@@ -30,9 +32,11 @@ export const Header: React.FC<Props> = ({ className }) => {
         </Link>
 
         {/* Center */}
-        <div className="mx-10 flex-1">
-          <SearchInput />
-        </div>
+        {hasSearch && (
+          <div className="mx-10 flex-1">
+            <SearchInput />
+          </div>
+        )}
 
         {/* Right side */}
         <div className="flex items-center gap-3">
@@ -40,9 +44,11 @@ export const Header: React.FC<Props> = ({ className }) => {
             <User size={16} />
             Log In
           </Button>
-          <div>
-            <CartButton />
-          </div>
+          {hasCart && (
+            <div>
+              <CartButton />
+            </div>
+          )}
         </div>
       </Container>
     </header>
