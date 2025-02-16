f6c1cee79e3c210ee236b005f810a39a201276a9
Zinger Marat <zingermarat@gmail.com>
2025-02-16 19:41:27 +0200

    Refactor CheckoutAddressForm to use FormInput for address field

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { WhiteBlock } from '../white-block';
 import { Input, Textarea } from '../../ui';
-import { FormTextarea } from '../form';
+import { FormInput, FormTextarea } from '../form';
 
 interface Props {
     className?: string;
 }
 
 export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
+  
     return (
         <WhiteBlock title="Delivery information">
         <div className="flex flex-col gap-5">
-          <Input name="Address" className="text-base" placeholder="Address" />
+          <FormInput name="address" className="text-base" placeholder="Address" />
           <FormTextarea name="comment" className="text-base" placeholder="Comment" rows={5} />
         </div>
       </WhiteBlock>
