1892e21aac1884a31d4ae7c47ea650e2174cadab
Zinger Marat <zingermarat@gmail.com>
2025-03-13 13:17:24 +0200

    Wrap Header component in Suspense for improved loading behavior; standardize 'use client' directive syntax

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,5 +1,6 @@
 import { Container, Header } from "@/shared/components/shared";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Next pizza | Checkout",
@@ -10,7 +11,9 @@ export default function CheckoutLayout({ children }: { children: React.ReactNode
   return (
     <main className="min-h-screen bg-[#F4F1EE]">
       <Container>
-        <Header className="border-gray-200" hasSearch={false} hasCart={false} />
+        <Suspense>
+          <Header className="border-gray-200" hasSearch={false} hasCart={false} />
+        </Suspense>
         {children}
       </Container>
     </main>
--- shared/components/shared/form/form-input.tsx
+++ shared/components/shared/form/form-input.tsx
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import { useFormContext } from "react-hook-form";
 import { Input } from "../../ui/input";
